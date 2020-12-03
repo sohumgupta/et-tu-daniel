d76e297b895f78825f3ae37bdd82212809f95085
ryanhkim <ryanhkim00@gmail.com>
2020-12-02 22:07:41 -0500

    updated model preprocess function

--- code/model_preprocess.py
+++ code/model_preprocess.py
@@ -4,7 +4,28 @@ START_TOKEN = "*start*"
 UNK_TOKEN = "*unk*"
 WINDOW_SIZE = 14
 
-from preprocess import pad_sentences, get_words, construct_vocab
+from preprocess import get_words, construct_vocab
+
+def pad_sentences(modern, original, sentence_length):
+	MODERN_padded_sentences = []
+	MODERN_sentence_lengths = []
+	for line in modern:
+		padded_MODERN = line[:sentence_length]
+		padded_MODERN += [STOP_TOKEN] + [PAD_TOKEN] * (sentence_length - len(padded_MODERN)-1)
+		MODERN_padded_sentences.append(padded_MODERN)
+
+	ORIGINAL_padded_sentences = []
+	ORIGINAL_sentence_lengths = []
+	for line in original:
+		padded_ORIGINAL = line[:sentence_length]
+		padded_ORIGINAL = [START_TOKEN] + padded_ORIGINAL + [STOP_TOKEN] + [PAD_TOKEN] * (sentence_length - len(padded_ORIGINAL)-1)
+		ORIGINAL_padded_sentences.append(padded_ORIGINAL)
+
+	return MODERN_padded_sentences, ORIGINAL_padded_sentences
+
+
+def convert_to_id(vocab, sentences):
+	return np.stack([[vocab[word] if word in vocab else vocab[UNK_TOKEN] for word in sentence] for sentence in sentences])
 
 # slightly modified from 'preprocess.py'
 def get_sentences(file_path):
@@ -18,27 +39,34 @@ def get_sentences(file_path):
 	return sentences, max_length
 
 def preprocess(modern_train, original_train, modern_test, original_test):
+	"""
+	Input: respective file paths
+	Output: modern_train_idx, modern_test_idx, original_train_idx, original_test_idx, vocab, idx
+	"""
 	# train
 	modern_train_sentences, modern_train_length = get_sentences(modern_train)
 	original_train_sentences, original_train_length = get_sentences(original_train)
-
 	max_train_length = max(modern_train_length, original_train_length)
-
-	modern_train_sentences = pad_sentences(modern_train_sentences, max_train_length)
-	original_train_sentences = pad_sentences(original_train_sentences, max_train_length)
 
 	# test
 	modern_test_sentences, modern_test_length = get_sentences(modern_test)
 	original_test_sentences, original_test_length = get_sentences(original_test)
-
 	max_test_length = max(modern_test_length, original_test_length)
 
-	modern_test_sentences = pad_sentences(modern_test_sentences, max_test_length)
-	original_test_sentences = pad_sentences(original_test_sentences, max_test_length)
+	# padding sentences
+	modern_train_sentences, original_train_sentences = pad_sentences(modern_train_sentences, original_train_sentences, max_train_length)
+	modern_test_sentences, original_test_sentences = pad_sentences(modern_test_sentences, original_test_sentences, max_test_length)
 
-	modern_vocab, modern_idx = construct_vocab(modern_train_sentences)
-	original_vocab, original_idx = construct_vocab(original_train_sentences)
+	# constructing vocab
+	vocab, idx = construct_vocab(modern_train_sentences + original_train_sentences)
 
-	return modern_train_sentences, original_train_sentences, modern_test_sentences, original_test_sentences
-# get_data("../data/test_modern.txt", "../data/test_original.txt")
+	# constructing IDs
+	modern_train_idx = convert_to_id(vocab, modern_train_sentences)
+	modern_test_idx = convert_to_id(vocab, modern_test_sentences)
 
+	original_train_idx = convert_to_id(vocab, original_train_sentences)
+	original_test_idx = convert_to_id(vocab, original_test_sentences)
+
+	return modern_train_idx, modern_test_idx, original_train_idx, original_test_idx, vocab, idx
+
+preprocess("../data/train_modern.txt", "../data/train_original.txt", "../data/test_modern.txt", "../data/test_original.txt")
